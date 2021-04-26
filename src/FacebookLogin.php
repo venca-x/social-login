71f47d7d05279d72e90b036bc04878160d3998ea
vEnCa-X <vencax@gmail.com>
2021-04-26 19:23:54 +0200

    Fix login. Deprecated prameters

--- src/FacebookLogin.php
+++ src/FacebookLogin.php
@@ -14,116 +14,274 @@ use Nette;
  */
 class FacebookLogin extends BaseLogin
 {
-	//me permissions
+	/**
+	 * Deprecated
+	 */
 	public const ID = 'id';
 
+	/**
+	 * Deprecated
+	 */
 	public const ABOUT = 'about';
 
+	/**
+	 * Deprecated
+	 */
 	public const ADMIN_NOTES = 'admin_notes';
 
+	/**
+	 * Deprecated
+	 */
 	public const AGE_RANGE = 'age_range';
 
+	/**
+	 * Deprecated
+	 */
 	public const BIO = 'bio';
 
+	/**
+	 * Deprecated
+	 */
 	public const BIRTHDAY = 'birthday';
 
+	/**
+	 * Deprecated
+	 */
 	public const CONTEXT = 'context';
 
+	/**
+	 * Deprecated
+	 */
 	public const COVER = 'cover';
 
+	/**
+	 * Deprecated
+	 */
 	public const CURRENCY = 'currency';
 
+	/**
+	 * Deprecated
+	 */
 	public const DEVICES = 'devices';
 
+	/**
+	 * Deprecated
+	 */
 	public const EDUCATION = 'education';
 
+	/**
+	 * Deprecated
+	 */
 	public const EMAIL = 'email';
 
+	/**
+	 * Deprecated
+	 */
 	public const FAVORITE_ATHLETES = 'favorite_athletes';
 
+	/**
+	 * Deprecated
+	 */
 	public const FAVORITE_TEAMS = 'favorite_teams';
 
+	/**
+	 * Deprecated
+	 */
 	public const FIRST_NAME = 'first_name';
 
+	/**
+	 * Deprecated
+	 */
 	public const GENDER = 'gender';
 
+	/**
+	 * Deprecated
+	 */
 	public const HOMETOWN = 'hometown';
 
+	/**
+	 * Deprecated
+	 */
 	public const INSPIRATIONAL_PEOPLE = 'inspirational_people';
 
+	/**
+	 * Deprecated
+	 */
 	public const INSTALL_TYPE = 'install_type';
 
+	/**
+	 * Deprecated
+	 */
 	public const INSTALLED = 'installed';
 
+	/**
+	 * Deprecated
+	 */
 	public const INTERESTED_IN = 'interested_in';
 
+	/**
+	 * Deprecated
+	 */
 	public const IS_SHARED_LOGIN = 'is_shared_login';
 
+	/**
+	 * Deprecated
+	 */
 	public const IS_VERIFIED = 'is_verified';
 
+	/**
+	 * Deprecated
+	 */
 	public const LABELS = 'labels';
 
+	/**
+	 * Deprecated
+	 */
 	public const LANGUAGES = 'languages';
 
+	/**
+	 * Deprecated
+	 */
 	public const LAST_NAME = 'last_name';
 
+	/**
+	 * Deprecated
+	 */
 	public const LINK = 'link';
 
+	/**
+	 * Deprecated
+	 */
 	public const LOCALE = 'locale';
 
+	/**
+	 * Deprecated
+	 */
 	public const LOCATION = 'location';
 
+	/**
+	 * Deprecated
+	 */
 	public const MEETING_FOR = 'meeting_for';
 
+	/**
+	 * Deprecated
+	 */
 	public const MIDDLE_NAME = 'middle_name';
 
+	/**
+	 * Deprecated
+	 */
 	public const NAME = 'name';
 
+	/**
+	 * Deprecated
+	 */
 	public const NAME_FORMAT = 'name_format';
 
+	/**
+	 * Deprecated
+	 */
 	public const PAYMENT_PRICEPOINTS = 'payment_pricepoints';
 
+	/**
+	 * Deprecated
+	 */
 	public const POLITICAL = 'political';
 
+	/**
+	 * Deprecated
+	 */
 	public const PUBLIC_KEY = 'public_key';
 
+	/**
+	 * Deprecated
+	 */
 	public const QUOTES = 'quotes';
 
+	/**
+	 * Deprecated
+	 */
 	public const RELATIONSHIP_STATUS = 'relationship_status';
 
+	/**
+	 * Deprecated
+	 */
 	public const RELIGION = 'religion';
 
+	/**
+	 * Deprecated
+	 */
 	public const SECURITY_SETTINGS = 'security_settings';
 
+	/**
+	 * Deprecated
+	 */
 	public const SHARED_LOGIN_UPGRADE_REQUIRED_BY = 'shared_login_upgrade_required_by';
 
+	/**
+	 * Deprecated
+	 */
 	public const SIGNIFICANT_OTHER = 'significant_other';
 
+	/**
+	 * Deprecated
+	 */
 	public const SPORTS = 'sports';
 
+	/**
+	 * Deprecated
+	 */
 	public const TEST_GROUP = 'test_group';
 
+	/**
+	 * Deprecated
+	 */
 	public const THIRD_PARTY_ID = 'third_party_id';
 
+	/**
+	 * Deprecated
+	 */
 	public const TIMEZONE = 'timezone';
 
+	/**
+	 * Deprecated
+	 */
 	public const TOKEN_FOR_BUSINESS = 'token_for_business';
 
+	/**
+	 * Deprecated
+	 */
 	public const UPDATED_TIME = 'updated_time';
 
+	/**
+	 * Deprecated
+	 */
 	public const VERIFIED = 'verified';
 
+	/**
+	 * Deprecated
+	 */
 	public const VIDEO_UPLOAD_LIMITS = 'video_upload_limits';
 
+	/**
+	 * Deprecated
+	 */
 	public const VIEWER_CAN_SEND_GIFT = 'viewer_can_send_gift';
 
+	/**
+	 * Deprecated
+	 */
 	public const WEBSITE = 'website';
 
+	/**
+	 * Deprecated
+	 */
 	public const WORK = 'work';
 
 	public const SOCIAL_NAME = 'facebook';
 
-	public const DEFAULT_FB_GRAPH_VERSION = 'v8.0';
+	public const DEFAULT_FB_GRAPH_VERSION = 'v10.0';
 
 	/** @var League\OAuth2\Client\Provider\Facebook */
 	private $provider;
@@ -140,7 +298,8 @@ class FacebookLogin extends BaseLogin
 		$cookieName,
 		Nette\Http\IResponse $httpResponse,
 		Nette\Http\IRequest $httpRequest
-	) {
+	)
+	{
 		$this->params = $params;
 		$this->cookieName = $cookieName;
 		$this->httpResponse = $httpResponse;
@@ -190,14 +349,14 @@ class FacebookLogin extends BaseLogin
 	 * @return array
 	 * @throws League\OAuth2\Client\Provider\Exception\IdentityProviderException
 	 */
-	public function getMe($fields)
+	public function getMe($fields = null)
 	{
-		$code = $this->httpRequest->getQuery('code');
-		if ($code !== null && $code == $_SESSION['oauth2state']) {
+		$state = $this->httpRequest->getQuery('state');
+		if ($state !== null && $state == $_SESSION['oauth2state']) {
 
 			// Try to get an access token (using the authorization code grant)
 			$token = $this->provider->getAccessToken('authorization_code', [
-				'code' => $code,
+				'code' => $this->httpRequest->getQuery('code'),
 			]);
 
 			try {
